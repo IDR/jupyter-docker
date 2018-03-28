d404cfb3abf155eacdf9fa2b5b5ea5380a8354a4
jmoore <josh@glencoesoftware.com>
2018-03-28 11:35:26 +0100

    Allow configuration of test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+IDR_IMAGE=${IDR_IMAGE:-test-jupyter-docker}
+
 set -eu
 set -x
-docker rm -f test-jupyter-docker || true
-docker build -t test-jupyter-docker .
-docker run -d --name test-jupyter-docker \
+docker rm -f ${IDR_IMAGE} || true
+docker build -t ${IDR_IMAGE} .
+docker run -d --name ${IDR_IMAGE} \
     -e IDR_HOST="$IDR_HOST" \
     -e IDR_USER="$IDR_USER" \
     -e IDR_PASSWORD="$IDR_PASSWORD" \
-    test-jupyter-docker
-docker cp test_notebooks.py test-jupyter-docker:/
-docker exec test-jupyter-docker /opt/conda/envs/python2/bin/pytest /test_notebooks.py
+    ${IDR_IMAGE}
+docker cp test_notebooks.py ${IDR_IMAGE}:/
+docker exec ${IDR_IMAGE} /opt/conda/envs/python2/bin/pytest "$@" /test_notebooks.py
